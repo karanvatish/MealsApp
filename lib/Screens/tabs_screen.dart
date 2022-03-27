60f7f07bb6bcee85acf3776f5e8a5ae16683ce07
Karan Sharma <karanvatish@gmail.com>
2022-03-27 23:28:37 +0530

    Added Bottom Navigation Bar

--- lib/Screens/tabs_screen.dart
+++ lib/Screens/tabs_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_const
+// ignore_for_file: unnecessary_const, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:meals_app/Screens/category_screen.dart';
@@ -12,21 +12,39 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {"page": CategoryScreen(), "title": "Categories"},
+    {"page": FavoriteScreen(), "title": "Your Favorites"}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _setSelectedPageIndex(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Meals App"),
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.category_rounded), text: "Category"),
-              Tab(icon: Icon(Icons.favorite), text: "Favorite"),
-            ],
-          ),
-        ),
-        body: TabBarView(children: [CategoryScreen(), FavoriteScreen()]),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meals App"),
+      ),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category_rounded), label: "Categories"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorite")
+        ],
+        backgroundColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedPageIndex,
+        onTap: _setSelectedPageIndex,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        elevation: 6,
       ),
     );
   }
